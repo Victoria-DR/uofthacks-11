a792890d27e21b3e896884fd9fd1822faf039797
Victoria <68347113+Victoria-DR@users.noreply.github.com>
2024-01-27 04:49:43 -0500

    Increase max request size

--- backend/app.js
+++ backend/app.js
@@ -11,12 +11,12 @@ const getAllEchoes = require('./routes/getAllEchoes');
 const getFriendEchoes = require('./routes/getFriendEchoes');
 const getEcho = require('./routes/getEcho');
 
-const getUser = require('./routes/getUser');
+const getUserImage = require('./routes/getUserImage');
 
 const shareEcho = require('./routes/shareEcho');
 
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: '1mb' }));
 app.use(cors());
 
 app.use("/v1/api", require("./routes/userRouter"));
@@ -30,7 +30,7 @@ app.post('/get-all-echoes', getAllEchoes);
 app.post('/get-friend-echoes', getFriendEchoes);
 app.post('/get-echo', getEcho);
 
-app.post('/get-user', getUser);
+app.post('/get-user-image', getUserImage);
 
 app.post('/share-echo', shareEcho);
 
