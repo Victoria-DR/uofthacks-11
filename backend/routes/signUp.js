bcf9c5ded63b81bdeb2aea51bc2a4c01c5d9114d
Victoria <68347113+Victoria-DR@users.noreply.github.com>
2024-01-27 05:22:08 -0500

    Fix sign up route

--- backend/routes/signUp.js
+++ backend/routes/signUp.js
@@ -3,17 +3,27 @@ const addEntity = require('../utils/addEntity');
 
 const signUp = async(req, res, next) => {
   const user = {
-    email: req.body.email,
-    name: req.body.name,
-    s3ImageKey: req.body.imageKey,
-    echoes: [],
-    friends: []
+    "user": {
+      "S": req.body.email
+    },
+    "name": {
+      "S": req.body.name
+    },
+    "s3ImageKey": {
+      "S": req.body.imageKey
+    },
+    "echoes": {
+      "L": []
+    },
+    "friends": {
+      "L": []
+    }
   };
 
   const s3Response = await uploadImage(req.body.image, process.env.AWS_S3_BUCKET_USERS, req.body.imageKey);
   const dynamoDBResponse = await addEntity(user, process.env.AWS_DYNAMODB_TABLE_USERS);
 
-  res.send(s3Response.statusCode === 200 && dynamoDBResponse.statusCode === 200);
+  res.send(dynamoDBResponse);
 };
 
 module.exports = signUp;
