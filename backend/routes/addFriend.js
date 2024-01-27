4bf4a6f8aaf4f42ff0ddd9c5043c80ac7867ad8a
Victoria <68347113+Victoria-DR@users.noreply.github.com>
2024-01-27 05:34:56 -0500

    Fix addFriend route key

--- backend/routes/addFriend.js
+++ backend/routes/addFriend.js
@@ -3,7 +3,9 @@ const updateEntity = require('../utils/updateEntity');
 const addFriend = async(req, res, next) => {
   const response = await updateEntity(
     {
-      user: req.body.email
+      "user": {
+        "S": req.body.email
+      }
     },
     process.env.AWS_DYNAMODB_TABLE_USERS,
     `SET friends = list_append(friends, :friend)`,
