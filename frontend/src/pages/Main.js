e7c4b50355b24781a43128ea13b4eab21b4f6fca
Jason Wang <40612523+jasonfyw@users.noreply.github.com>
2024-01-27 03:26:10 -0500

    refactor: extract into components

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -4,7 +4,7 @@ import Network from '../components/Network/Network';
 
 const Main = () => {
     return (
-        <Box w={'100vw'} h={'100vh'}>
+        <Box w={'100vw'} h={'100vh'} bg={'#010323'}>
             <Network />
         </Box>
     );
