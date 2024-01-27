b6afc983862055ba89c88549c6310c7ad8a90eb8
Jason Wang <40612523+jasonfyw@users.noreply.github.com>
2024-01-26 23:15:24 -0500

    init CRA frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+    ChakraProvider,
+    theme,
+} from '@chakra-ui/react';
+
+function App() {
+    return (
+        <ChakraProvider theme={theme}>
+            
+        </ChakraProvider>
+    );
+}
+
+export default App;
