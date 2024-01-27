ad960b887d80c18aac9a16dd2f40f0dfa7a3095d
Victoria <68347113+Victoria-DR@users.noreply.github.com>
2024-01-27 05:32:41 -0500

    Edit response message for signUp route

--- backend/routes/signUp.js
+++ backend/routes/signUp.js
@@ -23,7 +23,7 @@ const signUp = async(req, res, next) => {
   const s3Response = await uploadImage(req.body.image, process.env.AWS_S3_BUCKET_USERS, req.body.imageKey);
   const dynamoDBResponse = await addEntity(user, process.env.AWS_DYNAMODB_TABLE_USERS);
 
-  res.send(dynamoDBResponse);
+  res.send("success");
 };
 
 module.exports = signUp;
