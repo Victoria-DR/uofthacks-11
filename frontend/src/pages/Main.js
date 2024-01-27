b80a0be3819775e1537f8ff3ea44d0760dad3b13
Jason Wang <40612523+jasonfyw@users.noreply.github.com>
2024-01-27 01:46:49 -0500

    add: image rolodex template code

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Box } from '@chakra-ui/react';
+import Network from '../components/Network/Network';
 
 const Main = () => {
     return (
-        <div>
-            Main page
-        </div>
+        <Box w={'100vw'} h={'100vh'}>
+            <Network />
+        </Box>
     );
 };
 
