a19d1ab04c0254b0e1e8bdf1844de056d85ae207
Jason Wang <40612523+jasonfyw@users.noreply.github.com>
2024-01-26 23:32:01 -0500

    add: routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,15 @@ import {
     ChakraProvider,
     theme,
 } from '@chakra-ui/react';
+import { Route, Routes } from 'react-router';
+import Main from './pages/Main';
 
 function App() {
     return (
         <ChakraProvider theme={theme}>
-            
+            <Routes>
+                <Route path="/" element={<Main />} />
+            </Routes>
         </ChakraProvider>
     );
 }
