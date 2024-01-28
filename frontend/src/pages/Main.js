5b84431bfe33d198e66faad3c3d728e241673f99
Jason Wang <40612523+jasonfyw@users.noreply.github.com>
2024-01-28 01:46:34 -0500

    add: set profile node positions

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -2,10 +2,10 @@ import { React, useEffect } from 'react';
 import { Box } from '@chakra-ui/react';
 import { useAuth0 } from "@auth0/auth0-react";
 import Network from '../components/Network/Network';
-import UploadImage from '../components/UploadImage';
 import { profileData } from '../data/manualProfileData';
 import UserService from '../services/UserService';
 import { useUser } from '../contexts/UserContext';
+import { setProfilePositions } from '../helpers/graph.helpers';
 
 const Main = () => {
     const { user } = useAuth0();
@@ -19,7 +19,7 @@ const Main = () => {
 
     return (
         <Box w={'100%'} h={'100vh'} bg={'#15151f'}>
-            <Network profileData={profileData} />
+            <Network profileData={setProfilePositions(profileData)} />
             {/* <UploadImage /> */}
         </Box>
     );
