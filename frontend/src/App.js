b73e2e230e3e1039b520ecff440e9e6f8fbe565b
Victoria <68347113+Victoria-DR@users.noreply.github.com>
2024-01-27 21:12:28 -0500

    Load landing page when not authenticated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,16 +3,20 @@ import {
     ChakraProvider,
     theme,
 } from '@chakra-ui/react';
-import { Route, Routes } from 'react-router';
+import { useAuth0 } from "@auth0/auth0-react";
+import { Route, Routes, Navigate } from 'react-router';
 import Main from './pages/Main';
 import Landing from './pages/Landing';
 
 function App() {
+    const { user, isAuthenticated } = useAuth0();
+
     return (
         <ChakraProvider theme={theme}>
             <Routes>
                 <Route path="/" element={<Landing />} />
-                <Route path="/graph" element={<Main />} />
+                { isAuthenticated && <Route path="/graph" element={<Main />} />}
+                <Route path="*" element={<Landing />} />
             </Routes>
         </ChakraProvider>
     );
